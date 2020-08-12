18907ea1536ebd023283ed80918e53ceb87a0ea9
jessicafarias <jessica.farias.rosado@gmail.com>
2020-08-12 15:40:08 -0500

    Adding flash messages
    Adding flash.notice for article created
    Adding flash.notice for article deleted
    Adding flash.notice for article updated
    Adding index.html as root file
    Adding flash to application.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,12 +16,13 @@ class ArticlesController < ApplicationController
     def create
       @article = Article.new(article_params)
       @article.save
-
+      flash.notice = "Article '#{@article.title}' Created"
       redirect_to article_path(@article)
     end
 
     def destroy
       @article = Article.destroy(params[:id])
+      flash.notice = "Article '#{@article.title}' deleted"
       redirect_to articles_path
     end
 
@@ -32,7 +33,7 @@ class ArticlesController < ApplicationController
     def update
       @article = Article.find(params[:id])
       @article.update(article_params)
-
+      flash.notice = "Article '#{@article.title}' Updated"
       redirect_to article_path(@article)
     end
 end
