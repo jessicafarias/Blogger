22aaca29efce84bef8a89b18bb1b11f56fb3b5b6
aracelicaes <aracelicaes@gmail.com>
2020-08-12 15:11:34 -0500

    Add Edit & Update Links Added Edit & Update methods in article_controller Create _form html partial Refactored new & edit with _form partial. Edit show.html.erb with Edit & Update Links.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,5 +24,16 @@ class ArticlesController < ApplicationController
       @article = Article.destroy(params[:id])
       redirect_to articles_path
     end
+
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def update
+      @article = Article.find(params[:id])
+      @article.update(article_params)
+
+      redirect_to article_path(@article)
+    end
 end
 
