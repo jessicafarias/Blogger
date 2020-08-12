f15b43c485a7d5a5b8bb035f94e1dd5fc5ac3b62
jessicafarias <jessica.farias.rosado@gmail.com>
2020-08-12 12:39:08 -0500

    Adding create article
    Adding def create to controller
    Adding def new to cotroller
    Adding form to new.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,16 @@ class ArticlesController < ApplicationController
     def show
       @article = Article.find(params[:id])
     end
+
+    def new
+      @article = Article.new
+    end
+
+    def create
+      @article = Article.new
+      @article.title = params[:article][:title]
+      @article.body = params[:article][:body]
+      @article.save
+      redirect_to article_path(@article)
+    end
 end
