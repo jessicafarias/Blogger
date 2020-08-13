491beec483dac059f3a483693f9de9d6fd7fbe01
aracelicaes <aracelicaes@gmail.com>
2020-08-13 11:00:37 -0500

    Created Web-Based Comments Creating comment form partial and rendering. Adding Comment.new in articles_controller. Updating routes resources with comments. Updated Comments Views, adding addtl comments directory and form partial. Created comments_helper with comment_params method. Created 2 sample comments.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+  include CommentsHelper
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
+
+    @comment.save
+
+    redirect_to article_path(@comment.article)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   root to: 'articles#index'
 end
