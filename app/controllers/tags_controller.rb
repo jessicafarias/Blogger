9cf01231dcf45d2c08e319506d3121d4e43566f3
jessicafarias <jessica.farias.rosado@gmail.com>
2020-08-13 18:15:51 -0500

    Adding dependent :destroy to Tag model

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -10,18 +10,7 @@ class TagsController < ApplicationController
 
     def destroy
         @tag = Tag.find(params[:id])
-        
-        @tagging = Tagging.new
-        @tagging = Tagging.find(params[:id])
-           if @tagging.destroy
-               flash[:success] = 'Object was successfully deleted.'
-               redirect_to tags_path
-           else
-               flash[:error] = 'Something went wrong'
-               redirect_to tags_patalsjhasljhah
-           end
-        # @tag.destroy(params[:id])
-        # flash.notice = "Tag '#{@tag.name}' deleted"
-        # redirect_to tags_path
+        @tag.destroy
+        redirect_to tags_path
     end
 end
