d964a80c9457ec4399937fd91e7c5041ef79074e
jessicafarias <jessica.farias.rosado@gmail.com>
2020-08-13 12:40:50 -0500

    Create Relationships with articles/tag/taggins
    Create tag model
    Create taggings model
    Migrate tag and taggins model
    Create tag-articles relationship through taggins
    Create articles-tag relationshio through taggins
    Created test tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
 end
---
+++ app/models/tag.rb
@@ -0,0 +1,7 @@
+class Tag < ApplicationRecord
+    has_many :taggings
+    has_many :articles, through: :taggings
+end
+
+
+
